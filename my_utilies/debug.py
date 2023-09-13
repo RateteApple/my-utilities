e801dfbde66454ab90d5a3a7da51368884fbe6f1
Ratete <105982649+RateteApple@users.noreply.github.com>
2023-09-14 03:25:50 +0900

    Update debug.py

--- my_utilies/debug.py
+++ my_utilies/debug.py
@@ -9,26 +9,27 @@ logger = logging.getLogger(__name__)
 
 
 # メソッドの開始、終了、プロセス時間を表示するデコレータ
+def debug_log(func):
+    @wraps(func)
+    def inner_func(*args, **kwargs):
+        # 前処理
+        logger.debug(f"START: {func.__name__}")
+        start_time = time.time()
+        # メソッドの実行
+        result = func(*args, **kwargs)
+        # 後処理
+        end_time = time.time()
+        logger.debug(f"END: {func.__name__}")
+        logger.debug(f"PROCESS TIME: {end_time - start_time:.3f} sec")
+        # Return the return value
+        return result
+
+    return inner_func
+
+
+# クラス内のメソッドに対してデコレータを適用するデコレータ
 def apply_dubug_log(exclude=[]):
-    # メソッドに対して適用されるデコレータ
-    def debug_log(func):
-        @wraps(func)
-        def inner_func(*args, **kwargs):
-            # 前処理
-            logger.debug(f"START: {func.__name__}")
-            start_time = time.time()
-            # メソッドの実行
-            result = func(*args, **kwargs)
-            # 後処理
-            end_time = time.time()
-            logger.debug(f"END: {func.__name__}")
-            logger.debug(f"PROCESS TIME: {end_time - start_time:.3f} sec")
-            # Return the return value
-            return result
-
-        return inner_func
-
-    # メソッドに対してデコレータを適用する処理
+    # デコレータを適用する処理
     def decorate(cls):
         for name, fn in inspect.getmembers(cls):
             if name.startswith("__"):
